5c23dccfab597541128cf2df3695bc653f6816e3
Huneshwar97 <66106166+Huneshwar97@users.noreply.github.com>
2022-10-28 18:54:16 +0530

    Add files via upload

---
+++ Silver/intro_to_prefix/Subarray sum 2.cpp
@@ -0,0 +1,24 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> values(n);
+    for(int i=0;i<n;i++)
+        cin>>values[i];
+    ll sum=0;
+    map<ll,int> seen;
+    seen[0]++;
+    ll count =0;
+    for(int i=0;i<n;i++){
+        sum +=values[i];
+        count+=seen[sum-x];
+        seen[sum]++;
+    }
+    cout<<count;
+ 
+}
